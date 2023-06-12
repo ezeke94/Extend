fee4e52491ea30250f785c86aaad9e1be9c8a1bc
djs-singh <46212095+djs-singh@users.noreply.github.com>
2023-06-12 23:27:18 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,28 +1,39 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     var prompt = message.prompt;
+    console.log("In background")
     
     chrome.storage.sync.get(['apiKey'], function (result) {
       var apiKey = result.apiKey;
-  
-      fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      console.log("In background in Sync Get")
+      fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + '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'
+          'Authorization': 'Bearer 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' //+ apiKey
         },
         body: JSON.stringify({
-          prompt: 'top 10 movies',
-          max_tokens: 50
+          
+            "model": "gpt-3.5-turbo",
+            "messages": [
+              {
+                "role": "user",
+                "content": "Top 10 movies according to IMdB"
+            }
+          ]
+          
         })
       })
         .then(function (response) {
+          console.log("In background in Response Get")
           return response.json();
         })
         .then(function (data) {
-          var generatedText = data.choices[0].text;
+          console.log("In background in Res .then")
+          var generatedText = data.choices[0].message.content;
           sendResponse(generatedText);
         })
         .catch(function (error) {
+          console.log("In background in Error")
           console.log('Error:', error);
         });
     });
